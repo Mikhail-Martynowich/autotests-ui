86802c391150d29a3ed68d9829fcb9214942abdf
Mikhail-Martynowich <mmartynowich@gmail.com>
2026-06-07 23:36:19 +0300

    fix auth test

--- playwright_authorization.py
+++ playwright_authorization.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright, expect
 
-'''with sync_playwright() as playwright:
+with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
@@ -18,27 +18,8 @@ from playwright.sync_api import sync_playwright, expect
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
 
-    page.wait_for_timeout(5000)'''
-
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    email_input = page.locator("//input[@id=':r0:']")
-    email_input.fill("user.name@gmail.com")
-
-    username_input = page.locator("//input[@id=':r1:']")
-    username_input.fill("username")
-
-    password_input = page.locator("//input[@id=':r2:']")
-    password_input.fill("password")
-
-    registration_button = page.get_by_test_id("registration-page-registration-button")
-    registration_button.click()
-
-    header = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(header).to_be_visible()
     page.wait_for_timeout(5000)
+
+
 
 
